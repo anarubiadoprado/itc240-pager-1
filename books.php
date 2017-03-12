32ca983e62d8782630dd7c35a52385886b5d2d3f
anarubiadoprado <anarubia10hotmailcom@anarubia10hotmailcoms-MacBook-Air.local>
2017-03-11 18:37:05 -0800

    Adds pager code to books.php

--- books.php
+++ books.php
@@ -1,41 +1,73 @@
-<?php include "includes/config.php"?>
-<?php include "includes/header.php"?>
-<h1><?=$pageID?></h1>
-<?php	
-$iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+<?php
+/**
+ * demo_list_pager.php demonstrates a list page that paginates data across 
+ * multiple pages
+ * 
+ * This page uses a Pager class which processes a mysqli SQL statement 
+ * and spans records across multiple pages. 
+ * 
+ * @package nmPager
+ * @author Bill Newman <williamnewman@gmail.com>
+ * @version 3.2 2015/11/24
+ * @link http://www.newmanix.com/
+ * @license http://www.apache.org/licenses/LICENSE-2.0 v. 3.0
+ * @see MyAutoLoader.php
+ * @see Pager.php 
+ * @todo none
+ */
 
-$sql = 'select * from Books';
+require 'includes/config.php'; #provides configuration, pathing, error handling, db credentials 
+ 
+# SQL statement
+$sql = "select * from test_Customers";
+
+#Fills <title> tag  
+$title = 'Customer List/View/Pager';
+
+# END CONFIG AREA ---------------------------------------------------------- 
+
+include 'includes/header.php'; #header must appear before any HTML is printed by PHP
+?>
+<h3 align="center"><?=THIS_PAGE;?></h3>
+
+<p>This page demonstrates a List/View/Pager web application.</p>
+<p>It adds the <b>Pager</b> class to add pagination to our pages.</p>
+<p>Take the code from it to enable paging on your pages!</p>
+<?php
+#reference images for pager
+$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
+$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
+
+#Create a connection
+# connection comes first in mysqli (improved) function
+$iConn = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(myerror(__FILE__,__LINE__,mysqli_connect_error()));
 
 
-$result = mysqli_query($iConn, $sql);
+# Create instance of new 'pager' class
+$myPager = new Pager(2,'',$prev,$next,'');
+$sql = $myPager->loadSQL($sql,$iConn);  #load SQL, pass in existing connection, add offset
+$result = mysqli_query($iConn,$sql) or die(myerror(__FILE__,__LINE__,mysqli_error($iConn)));
 
-/*
-
-'  .  XXX  .  '
-
-*/
 if(mysqli_num_rows($result) > 0)
-{
-while($row = mysqli_fetch_assoc($result))
-{
-	echo '<p>';
-  echo 'Book Name: <b>' . $row['BookName'] . '       </b>  ';
-   echo 'Author Name: <b>' . $row['BookAuthor'] . '  </b>  ';
-   echo 'Year Release: <b>' . $row['YearRelease'] . '   </b>  ';
-	echo 'Genre: <b>' . $row['Genre'] . '  </b>     ';
-	echo '<a href="book_view.php?id=' . $row['BooksID'] . '">' . $row['BookName'] . '</a>';
-	echo ' </p>';
+{#records exist - process
+	if($myPager->showTotal()==1){$itemz = "customer";}else{$itemz = "customers";}  //deal with plural
+    echo '<p align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</p>';
+	while($row = mysqli_fetch_assoc($result))
+	{# process each row
+         echo '<p align="center">
+            <a href="' . VIRTUAL_PATH . 'customer_view.php?id=' . (int)$row['CustomerID'] . '">' . dbOut($row['FirstName']) . '</a>
+            </p>';
+	}
+	//the showNAV() method defaults to a div, which blows up in our design
+    echo $myPager->showNAV();//show pager if enough records 
+    
+    //the version below adds the optional bookends to remove the div design problem
+    //echo $myPager->showNAV('<p align="center">','</p>');
+}else{#no records
+    echo "<p align=center>What! No Customers?  There must be a mistake!!</p>";	
 }
-
-  }else {
-	echo '<p>There are currently no Books</p>';
-	
-}
-
 @mysqli_free_result($result);
 @mysqli_close($iConn);
 
-	
-		
+include 'includes/footer.php';
 ?>
-<?php include "includes/footer.php"?>
